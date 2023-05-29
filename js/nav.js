ad211bcf51c54425305825fd1c8f4d0c5e6094d7
Lxndrmrdjss <133441327+Lxndrmrdjss@users.noreply.github.com>
2023-05-29 16:46:29 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,7 @@ const createNav = () => {
             </div>
         </div>
         <ul class="links-container">
-            <li class="link-item"><a href="home.html" class="link">home</a></li>
+            <li class="link-item"><a href="index.html" class="link">home</a></li>
             <li class="link-item"><a href="product.html" class="link">product</a></li>
             <li class="link-item"><a href="about.html" class="link">about</a></li>
             <li class="link-item"><a href="contact.html" class="link">contact</a></li>
